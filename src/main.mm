7318b042e89bbb496ed0efa1aaf16bdfcca056d2
Chris Reed <creed@emmi.local>
2012-12-28 17:47:33 -0600

    Added source code for blinkcpu.

---
+++ src/main.mm
@@ -0,0 +1,86 @@
+//
+//  main.m
+//  blinkcpu
+//
+//  Created by Chris Reed on 12/18/12.
+//  Copyright (c) 2012 Chris Reed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MenuMeterCPUStats.h"
+#import "hidapi.h"
+#import "Blink1.h"
+#import <unistd.h>
+#import <stdlib.h>
+
+#define LOAD_UPDATE_INTERVAL (0.5)
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        int result = hid_init();
+        if (result < 0)
+        {
+            NSLog(@"Failed to init hid library (%d)", result);
+            return 1;
+        }
+        
+        MenuMeterCPUStats * stats = [[MenuMeterCPUStats alloc] init];
+        uint32_t cpuCount = [stats numberOfCPUs];
+        
+        Blink1 * blink = [[Blink1 alloc] init];
+        [blink enumerate];
+        
+        // Loop forever.
+        while (true)
+        {
+            @autoreleasepool
+            {
+                NSArray * load = [stats currentLoad];
+//                NSLog(@"load=%@", load);
+
+                float combinedTotal = 0.0;
+                float combinedSystem = 0.0;
+                float combinedUser = 0.0;
+                int cpuNum = 0;
+                
+                for (NSDictionary * loadInfo in load)
+                {
+                    
+                    float system = [[loadInfo objectForKey:@"system"] floatValue];
+                    float user = [[loadInfo objectForKey:@"user"] floatValue];
+                    float total = system + user;
+//                    NSLog(@"cpu%d: system=%f, user=%f, total=%f", cpuNum, system, user, total);
+                    
+                    combinedSystem += system;
+                    combinedUser += user;
+                    combinedTotal += total;
+                    
+                    ++cpuNum;
+                }
+                
+                combinedSystem /= (float)cpuCount;
+                combinedUser /= (float)cpuCount;;
+                combinedTotal /= (float)cpuCount;
+                
+//                NSLog(@"combined:system=%f,user=%f,total=%f", combinedSystem, combinedUser, combinedTotal);
+                
+                combinedTotal = MAX(0.07, combinedTotal);
+                
+                NSColor * loadColor;
+                loadColor = [NSColor colorWithCalibratedRed:combinedTotal green:0.0 blue:0.0 alpha:1.0];
+                
+//                float hue = combinedTotal;
+//                loadColor = [NSColor colorWithCalibratedHue:1.0 saturation:1.0 brightness:combinedTotal alpha:1.0];
+                
+                [blink fadeToRGB:loadColor atTime:LOAD_UPDATE_INTERVAL];
+            }
+                
+            [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:LOAD_UPDATE_INTERVAL]];
+        }
+    }
+    
+    return 0;
+}
+
